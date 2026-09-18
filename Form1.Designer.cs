bca0b6c056bb3218a7a65919d640ae836f11f3cb
Anna <annacarolsechin@gmail.com>
2026-09-18 20:19:32 -0300

    style: Estilizando forms em si

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,9 +48,11 @@
             // lblNomeDoce
             // 
             lblNomeDoce.AutoSize = true;
-            lblNomeDoce.Location = new Point(121, 138);
+            lblNomeDoce.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblNomeDoce.ForeColor = Color.White;
+            lblNomeDoce.Location = new Point(94, 138);
             lblNomeDoce.Name = "lblNomeDoce";
-            lblNomeDoce.Size = new Size(92, 20);
+            lblNomeDoce.Size = new Size(109, 20);
             lblNomeDoce.TabIndex = 0;
             lblNomeDoce.Text = "Nome Doce:";
             lblNomeDoce.Click += lblNomeDoce_Click;
@@ -58,9 +60,11 @@
             // lblPrecoUnitario
             // 
             lblPrecoUnitario.AutoSize = true;
-            lblPrecoUnitario.Location = new Point(121, 177);
+            lblPrecoUnitario.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblPrecoUnitario.ForeColor = Color.White;
+            lblPrecoUnitario.Location = new Point(94, 177);
             lblPrecoUnitario.Name = "lblPrecoUnitario";
-            lblPrecoUnitario.Size = new Size(106, 20);
+            lblPrecoUnitario.Size = new Size(132, 20);
             lblPrecoUnitario.TabIndex = 1;
             lblPrecoUnitario.Text = "Preço Unitário:";
             lblPrecoUnitario.Click += lblPrecoUnitario_Click;
@@ -68,9 +72,11 @@
             // lblQuantidade
             // 
             lblQuantidade.AutoSize = true;
-            lblQuantidade.Location = new Point(121, 221);
+            lblQuantidade.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblQuantidade.ForeColor = Color.White;
+            lblQuantidade.Location = new Point(94, 221);
             lblQuantidade.Name = "lblQuantidade";
-            lblQuantidade.Size = new Size(90, 20);
+            lblQuantidade.Size = new Size(110, 20);
             lblQuantidade.TabIndex = 2;
             lblQuantidade.Text = "Quantidade:";
             lblQuantidade.Click += lblQuantidade_Click;
@@ -78,9 +84,11 @@
             // lblIdadeCliente
             // 
             lblIdadeCliente.AutoSize = true;
-            lblIdadeCliente.Location = new Point(119, 264);
+            lblIdadeCliente.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblIdadeCliente.ForeColor = Color.White;
+            lblIdadeCliente.Location = new Point(92, 264);
             lblIdadeCliente.Name = "lblIdadeCliente";
-            lblIdadeCliente.Size = new Size(100, 20);
+            lblIdadeCliente.Size = new Size(124, 20);
             lblIdadeCliente.TabIndex = 3;
             lblIdadeCliente.Text = "Idade Cliente:";
             lblIdadeCliente.Click += lblIdadeCliente_Click;
@@ -120,71 +128,86 @@
             // lblNome
             // 
             lblNome.AutoSize = true;
+            lblNome.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblNome.ForeColor = Color.White;
             lblNome.Location = new Point(412, 134);
             lblNome.Name = "lblNome";
-            lblNome.Size = new Size(50, 20);
+            lblNome.Size = new Size(57, 20);
             lblNome.TabIndex = 8;
             lblNome.Text = "Nome";
             // 
             // lblDesconto
             // 
             lblDesconto.AutoSize = true;
+            lblDesconto.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblDesconto.ForeColor = Color.White;
             lblDesconto.Location = new Point(412, 177);
             lblDesconto.Name = "lblDesconto";
-            lblDesconto.Size = new Size(72, 20);
+            lblDesconto.Size = new Size(87, 20);
             lblDesconto.TabIndex = 9;
             lblDesconto.Text = "Desconto";
             // 
             // lblValorFinal
             // 
             lblValorFinal.AutoSize = true;
+            lblValorFinal.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblValorFinal.ForeColor = Color.White;
             lblValorFinal.Location = new Point(412, 224);
             lblValorFinal.Name = "lblValorFinal";
-            lblValorFinal.Size = new Size(78, 20);
+            lblValorFinal.Size = new Size(97, 20);
             lblValorFinal.TabIndex = 10;
             lblValorFinal.Text = "Valor Final";
             // 
             // lblParcela
             // 
             lblParcela.AutoSize = true;
+            lblParcela.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblParcela.ForeColor = Color.White;
             lblParcela.Location = new Point(413, 264);
             lblParcela.Name = "lblParcela";
-            lblParcela.Size = new Size(56, 20);
+            lblParcela.Size = new Size(72, 20);
             lblParcela.TabIndex = 11;
             lblParcela.Text = "Parcela";
             // 
             // lblStatus
             // 
             lblStatus.AutoSize = true;
+            lblStatus.Font = new Font("Arial Rounded MT Bold", 10.2F, FontStyle.Italic);
+            lblStatus.ForeColor = Color.White;
             lblStatus.Location = new Point(413, 307);
             lblStatus.Name = "lblStatus";
-            lblStatus.Size = new Size(49, 20);
+            lblStatus.Size = new Size(61, 20);
             lblStatus.TabIndex = 12;
             lblStatus.Text = "Status";
             // 
             // btnCalcular
             // 
+            btnCalcular.BackColor = Color.Transparent;
+            btnCalcular.ForeColor = Color.RosyBrown;
             btnCalcular.Location = new Point(183, 329);
             btnCalcular.Name = "btnCalcular";
             btnCalcular.Size = new Size(94, 29);
             btnCalcular.TabIndex = 13;
             btnCalcular.Text = "Calcular";
-            btnCalcular.UseVisualStyleBackColor = true;
+            btnCalcular.UseVisualStyleBackColor = false;
             btnCalcular.Click += btnCalcular_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(255, 40);
+            label1.Font = new Font("Wide Latin", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(113, 45);
             label1.Name = "label1";
-            label1.Size = new Size(278, 40);
+            label1.Size = new Size(608, 42);
             label1.TabIndex = 14;
-            label1.Text = "                   Loja de Doces !\r\nInsira abaixo as informações necessárias:";
+            label1.Text = "                                     Loja de Doces !\r\nInsira abaixo as informações necessárias:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.RosyBrown;
             ClientSize = new Size(800, 450);
             Controls.Add(label1);
             Controls.Add(btnCalcular);
@@ -202,7 +225,7 @@
             Controls.Add(lblPrecoUnitario);
             Controls.Add(lblNomeDoce);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Loja Doces!";
             Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
